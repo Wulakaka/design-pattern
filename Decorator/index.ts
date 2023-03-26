f548b2d076952bcf9f9610ba7b3dfe9acca499f4
zhangweifeng <zhang.weifeng@tslsmart.com>
2023-03-26 23:45:42 +0800

    feat(Decorator): add example

--- Decorator/index.ts
+++ Decorator/index.ts
@@ -16,15 +16,36 @@ class Student extends Person{
 }
 
 abstract class Decorator extends Person{
-    protected person: Person;
+    protected person!: Person;
+}
+
+class DecoratorA extends Decorator{
     constructor(person: Person) {
         super();
         this.person = person
     }
     Operation() {
         this.person.Operation()
+        console.log('写作业')
     }
 }
 
-const zhangsan = new Student('张三')
+class DecoratorB extends Decorator{
+    constructor(person: Person) {
+        super();
+        this.person = person
+    }
+    Operation() {
+        this.person.Operation()
+        console.log('考试')
+    }
+}
+
+let zhangsan: Person = new Student('张三')
+
+zhangsan = new DecoratorA(zhangsan)
+zhangsan = new DecoratorB(zhangsan)
 zhangsan.Operation()
+
+const lisi = new DecoratorB(new DecoratorA(new Student('李四')))
+lisi.Operation()
